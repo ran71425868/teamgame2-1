215d1c435715c8ffcc6256044595054b2e3eede0
kokokoko1220 <2240094@ecc.ac.jp>
2025-05-23 11:46:17 +0900

    block

---
+++ teamgame/Assets/saymb/Warp.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//public class Warp : MonoBehaviour
+//{
+//    [SerializeField]
+//    private Transform outpoint;
+
+//    private void OnTriggerEnter(Collider other)
+//    {
+//        if (outpoint == null) return;
+//        Player player = other.GetComponent<Player>();
+//        if (player != null)
+//        {
+//            player.Warp(outpoint.position);
+//        }
+//    }
+//}
